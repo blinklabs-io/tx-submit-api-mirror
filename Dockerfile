764aefaa0f83179b72c01c2aa1d7fce29d7af82f
Andrew Gaffney <andrew@agaffney.org>
2022-04-24 08:30:49 -0500

    Initial implementation

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM golang:1.17 AS build
+
+COPY . /code
+
+WORKDIR /code
+
+RUN make build
+
+FROM ubuntu:bionic
+
+COPY --from=build /code/tx-submit-api-mirror /usr/local/bin/
+
+ENTRYPOINT ["/usr/local/bin/tx-submit-api-mirror"]
---
+++ config/config.go
@@ -0,0 +1,61 @@
+package config
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+)
+
+type Config struct {
+	Logging  LoggingConfig `yaml:"logging"`
+	Api      ApiConfig     `yaml:"api"`
+	Backends []string      `yaml:"backends" envconfig:"BACKENDS"`
+}
+
+type LoggingConfig struct {
+	Level string `yaml:"level" envconfig:"LOGGING_LEVEL"`
+}
+
+type ApiConfig struct {
+	ListenAddress string `yaml:"address" envconfig:"API_LISTEN_ADDRESS"`
+	ListenPort    uint   `yaml:"port" envconfig:"API_LISTEN_PORT"`
+}
+
+// Singleton config instance with default values
+var globalConfig = &Config{
+	Logging: LoggingConfig{
+		Level: "info",
+	},
+	Api: ApiConfig{
+		ListenAddress: "",
+		ListenPort:    8090,
+	},
+}
+
+func Load(configFile string) (*Config, error) {
+	// Load config file as YAML if provided
+	if configFile != "" {
+		buf, err := ioutil.ReadFile(configFile)
+		if err != nil {
+			return nil, fmt.Errorf("error reading config file: %s", err)
+		}
+		err = yaml.Unmarshal(buf, globalConfig)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing config file: %s", err)
+		}
+	}
+	// Load config values from environment variables
+	// We use "dummy" as the app name here to (mostly) prevent picking up env
+	// vars that we hadn't explicitly specified in annotations above
+	err := envconfig.Process("dummy", globalConfig)
+	if err != nil {
+		return nil, fmt.Errorf("error processing environment: %s", err)
+	}
+	return globalConfig, nil
+}
+
+// Return global config instance
+func GetConfig() *Config {
+	return globalConfig
+}
